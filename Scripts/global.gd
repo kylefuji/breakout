190aeff547f90b360612b8fb9590f3ccc4b98a5c
kylefuji <kylefuji63@gmail.com>
2026-02-04 22:17:55 -0700

    eod commit

---
+++ Scripts/global.gd
@@ -0,0 +1,34 @@
+extends Node
+
+var player_score:int = 0
+var is_paused:bool = false
+
+var menuScene:Node2D
+var gameScene:Node2D
+
+var menuOpenNoise:AudioStreamPlayer
+var menuCloseNoise:AudioStreamPlayer
+var ball:CharacterBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	menuScene = get_tree().get_root().get_node("Main/Menu")
+	gameScene = get_tree().get_root().get_node("Main/Game")
+	ball = gameScene.get_node("Ball")
+	
+func toggle_pause() -> void:
+	is_paused = not is_paused
+	get_tree().paused = not get_tree().paused 
+
+
+func toggle_menu() -> void:
+	toggle_pause()
+	if is_paused:
+		menuOpenNoise.play()
+		menuScene.start()
+		gameScene.hide()
+	else:
+		menuCloseNoise.play()
+		menuScene.hide()
+		gameScene.show()
--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,9 +3,16 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS 
+
+
+func _on_play_button_pressed() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_restart_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
